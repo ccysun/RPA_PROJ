0bc0226d8bf3e48a47b05f5afc0a00f3fe48a346
ccysun <cy.sun@batem.com>
2021-09-02 17:10:45 +0900

    BA-Studio_Tip delete

--- BA-Studio_Tip/아래한글_자동화_API/아래한글자동화샘플/read_text_hwp.py
+++
@@ -1,6 +0,0 @@
-import olefile
-
-f = olefile.OleFileIO('C:\\temp2\\아래한글예제\\사령장.hwp')
-encoded_text = f.openstream('PrvText').read()
-decoded_text = encoded_text.decode('UTF-16')
-print(decoded_text)
